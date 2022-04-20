77f4af98213042c4edda47efb352f55713ec1cf3
Hemanth Parisa <hemanthkumarparisa@gmail.com>
2022-04-20 16:21:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+import './App.css';
+import HeaderComponent from './components/header'
+
+function App() {
+  return (
+      <div>
+          <HeaderComponent />
+      </div>
+  );
+}
+
+export default App
+
+{/* <div className='header'>
+<div>Logo</div>
+</div>
+<div className='header0'>
+<div>Link1</div>
+<div>Link2</div>
+<div>Link3</div>
+<div>Link4</div>
+<div>Link5</div>
+</div>
+        
+
+ */}
---
+++ src/components/header.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+const HeaderComponent = () =>{
+    return <div>
+        <div className='task'>
+
+        <div className='task1'>
+         <div>Logo</div>
+        </div>
+        <div className='task11'> 
+         <div>Link1</div>
+         <div>Link2</div>
+         <div>Link3</div>
+         <div>Link4</div>
+         <div>Link5</div>
+        </div>
+        </div> 
+
+        <div className='sec'>
+
+            <div className='sec1'>
+               <div><b>Intro</b></div>
+               <div>Some introduction lines<br/>about the website or<br/>company</div>
+            </div>
+            <div className='sec2'>
+                <div>Featured Project<br/>Slider</div>
+            </div>
+         </div>
+
+         <div className='radio'>
+             <div></div>
+             <div></div>
+             <div></div>
+             <div></div>
+             <div></div>
+         </div>
+
+         <div className='serv'>
+         <div>service</div>
+         <div>service</div>
+         <div>service</div>
+         </div>
+         <div className='serv1'>
+             <div>service</div>
+             <div>service</div>
+             <div>service</div>
+         </div>
+
+        <div className='twi'>
+            <div>Twitter feed</div> 
+        </div>
+       <div className='last'>
+       <div className='last1'>
+            <div>Link1</div>
+            <div>Link2</div>
+            <div>Link3</div>
+            <div>Link4</div>
+            <div>Link5</div>
+        </div>
+        <div className='last2'>
+            <div>copyrights info</div>
+        </div>
+       </div>
+             
+
+
+       
+
+
+        
+    </div>
+}
+
+export default HeaderComponent;
